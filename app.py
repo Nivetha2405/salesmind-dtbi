dae57a6e5910c3b97677726e972732a030117b9c
Nivetha2405 <niv24ha@gmail.com>
2026-03-01 21:07:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,12 @@ def reports():
 def login():
     return render_template('login.html')
 
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
+
+
 # CSV REPORT GENERATOR - FIXED ✅
 @app.route('/api/reports/<report_type>')
 def generate_report(report_type):
@@ -132,6 +138,7 @@ def simulation():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
